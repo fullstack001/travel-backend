c7bdd707b17b888eb5490e33a2d125e1f1e25207
fullstack001 <mr.dimas332@gmail.com>
2024-08-16 21:29:13 +0300

    update code

--- routes/api/resa.js
+++ routes/api/resa.js
@@ -3,10 +3,26 @@ const router = express.Router();
 
 import Resa from "../../models/Resa";
 
-router.get("/getresadata", async (req, res) => {
+router.post("/getresadata", async (req, res) => {
+  const { page = 1, limit = 10 } = req.body;
+  const pageNumber = parseInt(page, 10);
+  const pageSize = parseInt(limit, 10);
   try {
-    const resaData = await Resa.find();
-    res.json(resaData);
+    // Fetch data with pagination
+    const resaData = await Resa.find()
+      .skip((pageNumber - 1) * pageSize) // Skip to the correct page
+      .limit(pageSize); // Limit the number of records
+
+    // Count total documents
+    const totalDocuments = await Resa.countDocuments();
+
+    // Send response
+    res.json({
+      data: resaData,
+      currentPage: pageNumber,
+      totalPages: Math.ceil(totalDocuments / pageSize),
+      totalItems: totalDocuments,
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
