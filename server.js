90cdcb2e5c0fb0b12c67303b01e0624aea86bc41
fullstack001 <mr.dimas332@gmail.com>
2024-09-02 19:10:37 +0300

    test

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ limit: "2000mb", extended: false }));
 
 // // Serve static files from the dist directory
-// app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.use("/api/auth", auth);
 app.use("/api/admin", admin);
@@ -44,9 +44,9 @@ app.use("/api/driver", driver);
 app.use("/api/vehicle", vehicle);
 app.use("/api/guid", guid);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
