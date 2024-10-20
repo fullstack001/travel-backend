3d7a35b62764e6b40a30f85cb74fb2b76423c523
fullstack001 <mr.dimas332@gmail.com>
2024-10-20 20:39:27 +0200

    fix backend

--- config/createAdmin.js
+++ config/createAdmin.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/User";
-import { userList } from "./userList";
+// import { userList } from "./userList";
 
 const createAdmin = async () => {
   const name = "Admin";
@@ -21,15 +21,15 @@ const createAdmin = async () => {
   } else {
     console.log("Admin user already exists.");
   }
-  userList.forEach(async (user) => {
-    const userExists = await User.findOne({ email: user.email });
-    if (!userExists) {
-      const newUser = new User(user);
-      const salt = await bcrypt.genSalt(10);
-      newUser.password = await bcrypt.hash(user.password, salt);
-      await newUser.save();
-    }
-  });
+  // userList.forEach(async (user) => {
+  //   const userExists = await User.findOne({ email: user.email });
+  //   if (!userExists) {
+  //     const newUser = new User(user);
+  //     const salt = await bcrypt.genSalt(10);
+  //     newUser.password = await bcrypt.hash(user.password, salt);
+  //     await newUser.save();
+  //   }
+  // });
 
   // Check if admin user exists, if not, create one
 };
