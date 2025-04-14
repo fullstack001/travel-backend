9f1904aa171e45fafe889df049ee51cf1b85fa3d
smartdev <smartdev931104@gmail.com>
2025-04-14 17:47:55 +0300

    update change

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ import vehicle from "./routes/api/vehicle";
 import guid from "./routes/api/guid";
 import driverList from "./routes/api/driverList";
 import excursion from "./routes/api/excursion";
+import Resa from "./models/Resa.js"; // Update this import path based on your project structure
 
 dotenv.config();
 
@@ -28,6 +29,41 @@ const app = express();
 connectDB();
 createAdmin();
 // saveData();
+
+// // Find and update all documents
+// const updateServiceDates = async () => {
+//   try {
+//     const bulkOps = [];
+
+//     // Fetch all documents (you can batch this if you want for performance)
+//     const allResas = await Resa.find({}, { _id: 1, service_date: 1 });
+
+//     for (const resa of allResas) {
+//       if (resa.service_date) {
+//         const newDate = new Date(resa.service_date);
+//         newDate.setDate(newDate.getDate() + 1); // Add one day
+
+//         bulkOps.push({
+//           updateOne: {
+//             filter: { _id: resa._id },
+//             update: { $set: { service_date: newDate } },
+//           },
+//         });
+//       }
+//     }
+
+//     if (bulkOps.length > 0) {
+//       const result = await Resa.bulkWrite(bulkOps);
+//       console.log(`Updated ${result.modifiedCount} documents.`);
+//     } else {
+//       console.log("No documents with valid service_date found.");
+//     }
+//   } catch (err) {
+//     console.error("Error updating service_date:", err);
+//   }
+// };
+
+// updateServiceDates();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ limit: "2000mb", extended: false }));
